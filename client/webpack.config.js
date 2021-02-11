727640f25e0c8a92e8afdef256a784103d2f9a98
KenKaminsky <ken.k.kaminsky@gmail.com>
2021-02-11 23:26:58 +0000

    adding react client

---
+++ client/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const Configuration = require('webpack').Configuration;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const webpackConfig = (env) => ({
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.PRODUCTION': env.production || !env.development,
+      'process.env.NAME': JSON.stringify(require('./package.json').name),
+      'process.env.VERSION': JSON.stringify(require('./package.json').version),
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}', // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+      },
+    }),
+  ],
+});
+
+module.exports = webpackConfig;
